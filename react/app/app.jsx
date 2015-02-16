4f99e71036e8559d65099fd3ce9570a73c119a89
Chris Chua <chris@thousandeyes.com>
2015-02-15 18:03:09 -0800

    add annotations component

--- react/app/app.jsx
+++ react/app/app.jsx
@@ -31,12 +31,16 @@ var TimeArea = React.createClass({
       y: d3.scale.linear().range([HEIGHT, 0]),
     }; // create scale
     var data = [{x: 0, y: 0}, {x: 0.5, y: 0.5}];
+    var annotations = [
+      {x: 0.1, l: 0.2, fill: 'green'}
+    ];
 
     return (
       <div>
         <Tooltip tooltip={this.state.tooltip}></Tooltip>
         <svg width={WIDTH} height={HEIGHT}>
           <Timeline scale={scale} data={data} onTooltip={this.handleTooltip}></Timeline>
+          <Annotation scale={scale} data={annotations} onTooltip={this.handleTooltip}></Annotation>
         </svg>
       </div>
     );
@@ -71,6 +75,35 @@ var Timeline = React.createClass({
           height={this.props.scale.y.range()[0]}
           fill="transparent" />
         <path d={path}/>
+      </g>
+    );
+  },
+
+});
+
+var Annotation = React.createClass({
+
+  handleMouseMove: function(e) {
+    this.props.onTooltip({
+      text: 'X: ' + e.clientX
+    });
+  },
+
+  render: function() {
+    var x = this.props.scale.x; // create scale
+    var y = this.props.scale.y; // create scale
+
+    return (
+      <g onMouseMove={this.handleMouseMove} >
+        {this.props.data.map(function (d) {
+          return (
+            <rect x={x(d.x)}
+              width={x(d.x + d.l)}
+              y="0"
+              height={this.props.scale.y.range()[0]}
+              fill={d.fill} />
+          );
+        }, this)}
       </g>
     );
   },
